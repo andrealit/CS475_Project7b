b8b24cae9db19115e995a34faa795dda5e31b370
Andrea Tongsak <andy.st2001@gmail.com>
2022-06-03 21:12:09 -0700

    Update submit.bash

--- submit.bash
+++ submit.bash
@@ -3,8 +3,8 @@
 #SBATCH -J AutoCorr
 #SBATCH -A cs475-575
 #SBATCH -p class
-#SBATCH -N 4 # number of nodes
-#SBATCH -n 4 # number of tasks
+#SBATCH -N 14 # number of nodes
+#SBATCH -n 14 # number of tasks
 #SBATCH --constraint=ib
 #SBATCH -o autocorr.out
 #SBATCH -e autocorr.err
@@ -12,4 +12,4 @@
 #SBATCH --mail-user=tongsaka@oregonstate.edu
 module load openmpi
 mpic++ proj7.cpp -o autocorr -lm
-mpiexec -mca btl self,tcp -np 4 ./autocorr
+mpiexec -mca btl self,tcp -np 10 ./autocorr
